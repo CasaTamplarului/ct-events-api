6be388430a72d30b2b9905e785728707182b0121
Timo-Wolf <89921735+Timo-Wolf@users.noreply.github.com>
2024-03-18 09:11:22 +0000

    remove directus tables

--- db/migrate/20230818190125_rename_description_to_tag_line_on_events_translations.rb
+++
@@ -1,9 +0,0 @@
-class RenameDescriptionToTagLineOnEventsTranslations < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :events_translations, :description, :tag_line
-
-    execute <<-SQL
-      UPDATE "public"."directus_fields" SET "field" = 'tag_line' WHERE "collection" = 'events_translations' AND "field" = 'description';
-    SQL
-  end
-end
