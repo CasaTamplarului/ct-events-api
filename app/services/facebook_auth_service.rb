9f18d9052704319eee4358d6c219faa4b665b9ac
Timotei Chiciudean <timo@synthbit.io>
2026-05-29 13:31:06 +0100

    Add FacebookAuthService with Graph API token validation

---
+++ app/services/facebook_auth_service.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'json'
+
+class FacebookAuthService
+  class InvalidTokenError < StandardError; end
+
+  GRAPH_BASE = 'https://graph.facebook.com'
+
+  def self.call(access_token)
+    app_id     = Rails.application.credentials.dig(:auth, :facebook_app_id)
+    app_secret = Rails.application.credentials.dig(:auth, :facebook_app_secret)
+
+    validate_token!(access_token, app_id, app_secret)
+    fetch_user_data(access_token)
+  end
+
+  class << self
+    private
+
+    def validate_token!(access_token, app_id, app_secret)
+      url = URI("#{GRAPH_BASE}/debug_token")
+      url.query = URI.encode_www_form(
+        input_token: access_token,
+        access_token: "#{app_id}|#{app_secret}"
+      )
+      body = get_json!(url)
+      data = body['data'] || {}
+      raise InvalidTokenError, 'Token is invalid' unless data['is_valid'] && data['app_id'] == app_id
+    end
+
+    def fetch_user_data(access_token)
+      url = URI("#{GRAPH_BASE}/me")
+      url.query = URI.encode_www_form(
+        fields: 'id,email,first_name,last_name,picture.type(large)',
+        access_token: access_token
+      )
+      payload = get_json!(url)
+
+      {
+        uid:        payload['id'],
+        email:      payload['email'],
+        first_name: payload['first_name'].to_s,
+        last_name:  payload['last_name'].to_s,
+        avatar_url: payload.dig('picture', 'data', 'url')
+      }
+    end
+
+    def get_json!(url)
+      response = Net::HTTP.get_response(url)
+      raise InvalidTokenError, "Graph API error: #{response.code}" unless response.is_a?(Net::HTTPSuccess)
+
+      JSON.parse(response.body)
+    end
+  end
+end
---
+++ spec/services/facebook_auth_service_spec.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe FacebookAuthService do
+  let(:access_token) { 'test_access_token' }
+  let(:app_id) { 'test_app_id' }
+  let(:app_secret) { 'test_app_secret' }
+
+  let(:debug_token_url) do
+    "https://graph.facebook.com/debug_token?input_token=#{access_token}&access_token=#{app_id}%7C#{app_secret}"
+  end
+
+  let(:me_url) do
+    "https://graph.facebook.com/me?fields=id%2Cemail%2Cfirst_name%2Clast_name%2Cpicture.type%28large%29&access_token=#{access_token}"
+  end
+
+  before do
+    allow(Rails.application.credentials).to receive(:dig)
+      .with(:auth, :facebook_app_id).and_return(app_id)
+    allow(Rails.application.credentials).to receive(:dig)
+      .with(:auth, :facebook_app_secret).and_return(app_secret)
+  end
+
+  describe '.call' do
+    context 'with a valid token' do
+      before do
+        stub_request(:get, debug_token_url)
+          .to_return(
+            status: 200,
+            body: { data: { is_valid: true, app_id: app_id } }.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+        stub_request(:get, me_url)
+          .to_return(
+            status: 200,
+            body: {
+              id: 'fb-uid-123',
+              email: 'ion@example.com',
+              first_name: 'Ion',
+              last_name: 'Popescu',
+              picture: { data: { url: 'https://fb.com/photo.jpg' } }
+            }.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+      end
+
+      it 'returns user data hash' do
+        result = described_class.call(access_token)
+
+        expect(result[:uid]).to eq('fb-uid-123')
+        expect(result[:email]).to eq('ion@example.com')
+        expect(result[:first_name]).to eq('Ion')
+        expect(result[:last_name]).to eq('Popescu')
+        expect(result[:avatar_url]).to eq('https://fb.com/photo.jpg')
+      end
+    end
+
+    context 'with a nil email (phone-only Facebook account)' do
+      before do
+        stub_request(:get, debug_token_url)
+          .to_return(
+            status: 200,
+            body: { data: { is_valid: true, app_id: app_id } }.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+        stub_request(:get, me_url)
+          .to_return(
+            status: 200,
+            body: { id: 'fb-uid-456', first_name: 'Ion', last_name: 'Popescu',
+                    picture: { data: { url: nil } } }.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+      end
+
+      it 'returns nil email without raising' do
+        result = described_class.call(access_token)
+        expect(result[:email]).to be_nil
+      end
+    end
+
+    context 'when debug_token returns is_valid: false' do
+      before do
+        stub_request(:get, debug_token_url)
+          .to_return(
+            status: 200,
+            body: { data: { is_valid: false, app_id: app_id } }.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+      end
+
+      it 'raises InvalidTokenError' do
+        expect { described_class.call(access_token) }
+          .to raise_error(FacebookAuthService::InvalidTokenError)
+      end
+    end
+
+    context 'when debug_token returns wrong app_id' do
+      before do
+        stub_request(:get, debug_token_url)
+          .to_return(
+            status: 200,
+            body: { data: { is_valid: true, app_id: 'other_app' } }.to_json,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+      end
+
+      it 'raises InvalidTokenError' do
+        expect { described_class.call(access_token) }
+          .to raise_error(FacebookAuthService::InvalidTokenError)
+      end
+    end
+
+    context 'when Graph API returns non-2xx' do
+      before do
+        stub_request(:get, debug_token_url)
+          .to_return(status: 400, body: '{}',
+                     headers: { 'Content-Type' => 'application/json' })
+      end
+
+      it 'raises InvalidTokenError' do
+        expect { described_class.call(access_token) }
+          .to raise_error(FacebookAuthService::InvalidTokenError)
+      end
+    end
+  end
+end
