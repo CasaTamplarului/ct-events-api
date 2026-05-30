1af6e6a0944c6bc57b374dedfb0b5371f7c0dfc2
Timotei Chiciudean <timo@synthbit.io>
2026-05-30 23:36:10 +0100

    Fix RuboCop Rails/InverseOf offense on checked_in_by association

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -5,7 +5,8 @@ class Attendee < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :order, optional: true
   belongs_to :ticket, optional: true
-  belongs_to :checked_in_by, class_name: 'User', foreign_key: :checked_in_by_user_id, optional: true
+  belongs_to :checked_in_by, class_name: 'User', foreign_key: :checked_in_by_user_id, optional: true,
+                             inverse_of: false
 
   enum :dietary_preference, { no_preference: 0, vegetarian: 1, vegan: 2 }
 
