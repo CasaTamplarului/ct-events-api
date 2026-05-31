fb435018ab34a2127b934c4b1f2a80ac70d17b14
Timotei Chiciudean <timo@synthbit.io>
2026-05-31 06:42:14 +0100

    Extract ScanSerialisable concern from OrdersController

--- app/controllers/api/v1/scan/orders_controller.rb
+++ app/controllers/api/v1/scan/orders_controller.rb
@@ -5,13 +5,14 @@ module Api
     module Scan
       class OrdersController < ActionController::API
         include Authenticatable
+        include ScanSerialisable
 
         before_action :authenticate_user!
         before_action { require_permission!(:can_check_in_attendees) }
         before_action :set_order
 
         def show
-          render json: serialise_order
+          render json: serialise_order(@order)
         end
 
         def update
@@ -32,7 +33,7 @@ module Api
             update_attendee_checkins(update_params)
           end
 
-          render json: serialise_order
+          render json: serialise_order(@order)
         end
 
         private
@@ -57,34 +58,6 @@ module Api
                 attendee.update!(checked_in: false, checked_in_at: nil, checked_in_by_user_id: nil)
               end
             end
-          end
-
-          def serialise_order
-            attendees = @order.attendees
-                              .includes(:checked_in_by, ticket: :tickets_translations)
-                              .order(:id)
-            {
-              order_reference: @order.order_reference,
-              payment_status: @order.payment_status,
-              attendees: attendees.map { |a| serialise_attendee(a) }
-            }
-          end
-
-          def serialise_attendee(attendee)
-            by = attendee.checked_in_by
-            {
-              id: attendee.id,
-              first_name: attendee.first_name,
-              last_name: attendee.last_name,
-              email_address: attendee.email_address,
-              ticket_name: attendee.ticket
-                           &.tickets_translations
-                           &.find { |t| t.languages_code == 'ro-RO' }
-                           &.name,
-              checked_in: attendee.checked_in,
-              checked_in_at: attendee.checked_in_at,
-              checked_in_by: by ? "#{by.first_name} #{by.last_name}".strip : nil
-            }
           end
       end
     end
---
+++ app/controllers/concerns/scan_serialisable.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module ScanSerialisable
+  private
+
+    def serialise_order(order)
+      attendees = order.attendees
+                       .includes(:checked_in_by, ticket: :tickets_translations)
+                       .order(:id)
+      {
+        order_reference: order.order_reference,
+        payment_status: order.payment_status,
+        attendees: attendees.map { |a| serialise_attendee(a) }
+      }
+    end
+
+    def serialise_attendee(attendee)
+      by = attendee.checked_in_by
+      {
+        id: attendee.id,
+        first_name: attendee.first_name,
+        last_name: attendee.last_name,
+        email_address: attendee.email_address,
+        ticket_name: attendee.ticket
+                             &.tickets_translations
+                             &.find { |t| t.languages_code == 'ro-RO' }
+                             &.name,
+        checked_in: attendee.checked_in,
+        checked_in_at: attendee.checked_in_at,
+        checked_in_by: by ? "#{by.first_name} #{by.last_name}".strip : nil
+      }
+    end
+end
